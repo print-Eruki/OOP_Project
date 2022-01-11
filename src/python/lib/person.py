8efe59b1603876be3f03616e6bfcf436de4b3539
Ale <palejorniel@gmail.com>
2022-01-10 23:50:14 -0400

    Change the class person to a dataclass

--- src/python/lib/person.py
+++ src/python/lib/person.py
@@ -1,20 +1,11 @@
-class Person:
+from dataclasses import dataclass
 
-    def __init__(self,fname,lname,g,a,h,w):
-        self.first_name = fname
-        self.last_name = lname
-        self.gender = g
-        self.age = a
-        self.height = h
-        self.weight = w
-        
-    def displaydata(self):
-        print(self.first_name)
-        print(self.last_name )
-        print(self.gender )
-        print(self.age)
-        print(self.height)
-        print(self.weight)
-        
-    def talk():
-        pass
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    gender: str
+    age: int
+    height: float
+    weight: float
+    
