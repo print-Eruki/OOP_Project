88052610b256e48dd7cd0eb6f707f17363af7096
Ale <palejorniel@gmail.com>
2022-01-11 19:20:27 -0400

    Almost done with person class, need to add counter and ABCs

--- src/python/lib/person.py
+++ src/python/lib/person.py
@@ -2,10 +2,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Person:
+
     first_name: str
     last_name: str
     gender: str
     age: int
     height: float
     weight: float
+
     
+    def displaydata(self):
+        print(self.first_name)
+        print(self.last_name)
+        print(self.gender)
+        print(self.age)
+        print(self.height)
+        print(self.weight)
+
+    def talk(self):
+        pass
+        
