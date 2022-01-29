341ec7faa47f7828ffd29ddf6bf0bdf506c10cbb
Ale <palejorniel@gmail.com>
2022-01-29 14:52:38 -0400

    Finished the calculate method in student.py

--- src/python/lib/student.py
+++ src/python/lib/student.py
@@ -16,4 +16,10 @@ class Student(Person):
         print("Hello! I am {} {} . I am a student at {}, studying {}.".format(self.first_name, self.last_name, self.institution, self.major))
 
 
-    
+
+    def calculate(self,*argv):
+
+
+        self.final_grade = sum(argv)
+        self.final_grade /= len(argv)
+        print(self.final_grade)    
